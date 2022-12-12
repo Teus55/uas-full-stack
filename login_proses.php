1db64fd4f0b4974056c1e8cac5927474a3712451
Teus55 <115389214+Teus55@users.noreply.github.com>
2022-12-12 19:22:57 +0700

    Add files via upload

--- login_proses.php
+++ login_proses.php
@@ -4,8 +4,10 @@ header("Access-Control-Allow-Origin: *");
 require_once("class/login.php");
 $objLogin = new login();
 $idlogin = $objLogin->Login($_POST['username'], $_POST['password']);
-if ($idlogin == true) {
+$row = $idlogin->fetch_assoc();
+if ($row) {
     $_SESSION['username'] = $_POST['username'];
     echo "Berhasil";
-} else
+} else {
     echo "Gagal";
+}
